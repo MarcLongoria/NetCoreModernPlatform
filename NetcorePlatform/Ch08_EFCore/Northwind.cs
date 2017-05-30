c524cc1f39a7f9a1b3227491e1dae1456cb949a6
Marc Longoria <marclongoria@outlook.com>
2017-05-30 17:35:27 -0500

    Adding Category, Northwind, Product classes

---
+++ NetcorePlatform/Ch08_EFCore/Northwind.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.CS7
+{
+    public class Northwind : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+            @"Data Source=(localdb)\mssqllocaldb;" +
+            "Initial Catalog=Northwind;" +
+            "MultipleActiveResultSets=true;" +
+            "Integrated Security=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+.Property(category => category.CategoryName)
+                .IsRequired()
+                .HasMaxLength(40);
+        }
+    }
+}
---
+++ NetcorePlatform/Ch08_EFCore/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.CS7
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string ProductName { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal? UnitPrice { get; set; }
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+    }
+}
